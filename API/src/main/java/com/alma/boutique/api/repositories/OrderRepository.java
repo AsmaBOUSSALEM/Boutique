b4f53a800626f146d7f1bb244adc58724204cd94
asmaboussalem <asmaboussalem@gmail.com>
2016-11-26 12:56:36 +0100

    api repo interfaces

---
+++ API/src/main/java/com/alma/boutique/api/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.alma.boutique.api.repositories;
+
+/**
+ * Created by asmaboussalem on 16/11/2016.
+ */
+public interface OrderRepository extends com.alma.boutique.infra.repositories.OrderRepository{
+
+}
---
+++ API/src/main/java/com/alma/boutique/api/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.alma.boutique.api.repositories;
+
+
+/**
+ * Created by asmaboussalem on 16/11/2016.
+ */
+public interface ProductRepository extends com.alma.boutique.infra.repositories.ProductRepository{
+
+}
